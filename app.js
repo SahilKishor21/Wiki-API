43e9214e3e9d2190a20b68d23c8e8d1291f8239c
Sahil <sahil.21ug4021@iiitranchi.ac.in>
2023-09-23 02:12:15 +0530

    Updated

--- app.js
+++ app.js
@@ -123,6 +123,7 @@ app.route("/articles/:articleTitle")
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, () => {
+  console.log(`Successfully started server on port ${3000}.`);
 });
+
